d62d4eea756a20e0872fa2a7ee93618f25a73778
Krish <krish.z@nodereal.io>
2024-08-05 17:30:36 +0800

    fix check logic

--- op-node/rollup/derive/engine_controller.go
+++ op-node/rollup/derive/engine_controller.go
@@ -307,7 +307,12 @@ func (e *EngineController) checkForkchoiceUpdatedStatus(status eth.ExecutePayloa
 
 // checkELSyncTriggered checks returned err of engine_newPayloadV1
 func (e *EngineController) checkELSyncTriggered(status eth.ExecutePayloadStatus, err error) bool {
-	return e.syncMode != sync.ELSync && status == eth.ExecutionSyncing && strings.Contains(err.Error(), "forced head needed for startup")
+	if err == nil {
+		return false
+	} else if strings.Contains(err.Error(), "forced head needed for startup") {
+		return e.syncMode != sync.ELSync && status == eth.ExecutionSyncing
+	}
+	return false
 }
 
 // checkUpdateUnsafeHead checks if we can update current unsafeHead for op-node
@@ -421,13 +426,11 @@ func (e *EngineController) InsertUnsafePayload(ctx context.Context, envelope *et
 				log.Info("engine processed data successfully")
 				e.needFCUCall = false
 				needSyncWithEngine = true
-				log.Info("not return")
-				//return nil
+				break
 			} else {
 				return NewTemporaryError(fmt.Errorf("engine failed to process inconsistent data"))
 			}
 		}
-
 	}
 
 	if !e.checkNewPayloadStatus(status.Status) {
